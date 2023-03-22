786062270284b3b4035909df50260bb8508099f0
Hupan <fabrihupan@gmail.com>
2023-03-22 11:16:49 -0300

    JS-Redirecciones

--- js/main.js
+++ js/main.js
@@ -135,3 +135,40 @@ const productos = [
 //     },
 //     precio: ,
 // },
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+
+function cargarProductos() {
+
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto")
+        div.innerHTML=`
+            <img class="producto-imagen" src="${producto.img}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$ ${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}" >Agregar</button>
+            </div>
+        `
+
+        contenedorProductos.append(div);
+
+    })
+}
+
+cargarProductos();
+
+botonesCategorias.forEach(boton => {
+
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+
+        e.currentTarget.classList.add("active");
+
+    })
+
+})
