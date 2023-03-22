4d0f76b2ccc2f11dc3fb7422af0e3be892f52d65
Hupan <fabrihupan@gmail.com>
2023-03-22 09:48:59 -0300

    Empezando js

---
+++ js/main.js
@@ -0,0 +1,137 @@
+const productos = [
+    //ABRIGOS
+    {
+        id: "abrigo-01",
+        titulo: "Campera Chelsea",
+        img: "../img/Abrigos/Campera Chelsea.png",
+        categorias: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 45000
+    },
+    {
+        id: "abrigo-02",
+        titulo: "Campera Inflada Niño Niña Corderito Abrigada Kids Invierno",
+        img: "../img/Abrigos/Campera Inflada Niño Niña Corderito Abrigada Kids Invierno.png",
+        categorias: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 9999
+    },
+    {
+        id: "abrigo-03",
+        titulo: "Camperas De Neopren Hombre Liquidación",
+        img: "../img/Abrigos/Camperas De Neopren Hombre Liquidación.png",
+        categorias: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 12500
+    },
+    {
+        id: "abrigo-04",
+        titulo: "Tapado Mujer Capucha Ultra Liviana Importada ...!!!",
+        img: "../img/Abrigos/Tapado Mujer Capucha Ultra Liviana Importada ...!!!.png",
+        categorias: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 22990
+    },
+    // CAMISETAS
+    {
+        id: "camiseta-01",
+        titulo: "Camiseta Koi 2022 E-sports.png",
+        img: "../img/Camisetas/Camiseta Koi 2022 E-sports.png",
+        categorias: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 4550
+    },
+    {
+        id: "camiseta-02",
+        titulo: "Camiseta Musculosa Basquet Nba Bulls",
+        img: "../img/Camisetas/Camiseta Musculosa Basquet Nba Bulls.png",
+        categorias: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 3490
+    },
+    {
+        id: "camiseta-03",
+        titulo: "Camisetas G2 2023 E-sports",
+        img: "../img/Camisetas/Camisetas G2 2023 E-sports.png",
+        categorias: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        },
+        precio: 4450
+    },
+    // PANTALONES
+    {
+        id: "pantalon-01",
+        titulo: "Bombacha Campo Pampero",
+        img: "../img/Pantalones/Bombacha Campo Pampero.png",
+        categorias: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 4450
+    },
+    {
+        id: "pantalon-02",
+        titulo: "Bombacha De Campo Pampero Lisa - T38 A T60 - Uso Intensivo",
+        img: "../img/Pantalones/Bombacha De Campo Pampero Lisa - T38 A T60 - Uso Intensivo.png",
+        categorias: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 7800
+    },
+    {
+        id: "pantalon-03",
+        titulo: "Pantalon Cargo Gabardina Pre Lavada For Men Hard Work",
+        img: "../img/Pantalones/Pantalon Cargo Gabardina Pre Lavada For Men Hard Work.png",
+        categorias: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 7900
+    },
+    {
+        id: "pantalon-04",
+        titulo: "Pantalon Cargo Gabardina",
+        img: "../img/Pantalones/Pantalon Cargo Gabardina.png",
+        categorias: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 7890
+    },
+    {
+        id: "pantalon-05",
+        titulo: "Pantalón Cargo",
+        img: "../img/Pantalones/Pantalón Cargo.png",
+        categorias: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        },
+        precio: 24000
+    }
+];
+
+// MODELO
+// {
+//     id: ,
+//     titulo: ,
+//     img: ,
+//     categorias: {
+//         nombre: ,
+//         id: 
+//     },
+//     precio: ,
+// },
