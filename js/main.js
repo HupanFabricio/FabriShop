12b2ac6968bf209afaffb8a9ccc8fa545154c1fc
Hupan <fabrihupan@gmail.com>
2023-03-23 12:21:49 -0300

    LocalStorage

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
 //Funciones:
 function cargarProductos(productosElegidos) {
@@ -209,9 +210,23 @@ const productosEnCarrito = [];
 function agregarAlCarrito (e) {
 
     const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
 
-    const productoAgregado = productos.find(producto => producto.id === idBoton)
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton)
+        productosEnCarrito[index].cantidad ++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    };
 
-    productosEnCarrito.push(productoAgregado)
-    console.log(productosEnCarrito)
+    actualizarNumerito();
+
+    //localStorage
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acu, producto) => acu + producto.cantidad, 0);
+    numerito.innerHTML = nuevoNumerito;
 }
