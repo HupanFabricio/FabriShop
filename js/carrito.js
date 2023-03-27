30bd30432f51d159eec009c1464e26fbdd28468a
Hupan <fabrihupan@gmail.com>
2023-03-27 15:46:46 -0300

    Actualizacion de carro

--- js/carrito.js
+++ js/carrito.js
@@ -1,53 +1,123 @@
 //Array de productos que vienen desde el index!
-const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+productosEnCarrito = JSON.parse(productosEnCarrito);
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector(".carrito-acciones-vaciar");
+const contenedorTotal = document.querySelector("#total");
+const botonComprar = document.querySelector(".carrito-acciones-comprar");
 
-console.log(productosEnCarrito)
+// console.log(productosEnCarrito)
 
-if (productosEnCarrito) {
+function cargarProductosCarrito () {
 
-    contenedorCarritoVacio.classList.add("disabled");
-    contenedorCarritoProductos.classList.remove("disabled");
-    contenedorCarritoAcciones.classList.remove("disabled");
-    contenedorCarritoComprado.classList.add("disabled");
+    if (productosEnCarrito && productosEnCarrito.length > 0) {
+
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+        
+        contenedorCarritoProductos.innerHTML="";
+        
+        productosEnCarrito.forEach(producto => {
     
-    contenedorCarritoProductos.innerHTML="";
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+            <img class="carrito-producto-imagen" src="${producto.img}" alt="${producto.titulo}">
+            <div class="carrito-producto-titulo">
+                <small>Título</small>
+                <h3>${producto.titulo}</h3>
+            </div>
+            <div class="carrito-producto-cantidad">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="carrito-producto-precio">
+                <small>Precio</small>
+                <p>$ ${producto.precio}</p>
+            </div>
+            <div class="carrito-producto-subtotal">
+                <small>Subtotal</small>
+                <p>$ ${producto.precio * producto.cantidad}</p>
+            </div>
+            <button id="${producto.id}" class="carrito-producto-eliminar"><i class="bi bi-trash3"></i></button>
+            `;
     
-    productosEnCarrito.forEach(producto => {
+            contenedorCarritoProductos.append(div);
+    
+        })
+    
+    } else {
+    
+        contenedorCarritoVacio.classList.remove("disabled");
+        contenedorCarritoProductos.classList.add("disabled");
+        contenedorCarritoAcciones.classList.add("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    
+    }
 
-        const div = document.createElement("div");
-        div.classList.add("carrito-producto");
-        div.innerHTML = `
-        <img class="carrito-producto-imagen" src="${producto.img}" alt="${producto.titulo}">
-        <div class="carrito-producto-titulo">
-            <small>Título</small>
-            <h3>${producto.titulo}</h3>
-        </div>
-        <div class="carrito-producto-cantidad">
-            <small>Cantidad</small>
-            <p>${producto.cantidad}</p>
-        </div>
-        <div class="carrito-producto-precio">
-            <small>Precio</small>
-            <p>$ ${producto.precio}</p>
-        </div>
-        <div class="carrito-producto-subtotal">
-            <small>Subtotal</small>
-            <p>$ ${producto.precio * producto.cantidad}</p>
-        </div>
-        <button id="${producto.id}" class="carrito-producto-eliminar"><i class="bi bi-trash3"></i></button>
-        `;
-
-        contenedorCarritoProductos.append(div);
-
-    })
-
-} else {
-
-
+    actualizarBotonesEliminar();
+    actualizarTotal ();
 
 }
+
+cargarProductosCarrito();
+
+function actualizarBotonesEliminar() {
+
+    botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+
+    botonesEliminar.forEach(boton => {
+
+        boton.addEventListener("click", eliminarDelCarrito)
+
+    })
+}
+
+function eliminarDelCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton );
+
+    productosEnCarrito.splice(index,1);
+
+    cargarProductosCarrito(); 
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+
+function vaciarCarrito () {
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+    cargarProductosCarrito();
+}
+
+function actualizarTotal () {
+    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0)
+    contenedorTotal.innerText = `$ ${totalCalculado}`
+};
+
+botonComprar.addEventListener("click", comprarCarrito);
+
+function comprarCarrito () {
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.remove("disabled");
+}
+
+
+
